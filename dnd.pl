c1dbe504e720d72147ff0e7efe6a1949258d5f83
davidphood <30076632+davidphood@users.noreply.github.com>
2022-07-18 23:49:36 +1000

    Fixed formatting - Added spaces between arguments.

--- dnd.pl
+++ dnd.pl
@@ -1,12 +1,12 @@
 :- use_module(library(clpfd)).
 
-dnd(RowCounts,ColCounts,Rows) :-
-    length(RowCounts,NumRows),
-    length(ColCounts,NumCols),
-    build_board(NumRows,NumCols,Rows),
-    build_mega_board(RowCounts,ColCounts,Rows,_MegaRowCounts,_MegaColCounts,_MegaRows),
+dnd(RowCounts, ColCounts, Rows) :-
+    length(RowCounts, NumRows),
+    length(ColCounts, NumCols),
+    build_board(NumRows, NumCols, Rows),
+    build_mega_board(Rows, _MegaRows),
     % Build columns out of the Rows (instead of rows)
-    rule_lines(RowCounts,ColCounts,Rows).
+    rule_lines(RowCounts, ColCounts, Rows).
     % Work through the MegaRows. Use corners at:
     % [0,0], [0,1], [0,1], etc
     % [1,0], [1,1], etc
@@ -18,66 +18,62 @@ dnd(RowCounts,ColCounts,Rows) :-
     % - 2x2 rule for invalid corridors
 
 
-build_board(NumRows,NumCols,Board) :-
+build_board(NumRows, NumCols, Board) :-
     % Board is NumRows x NumCols
-    length(Board,NumRows),
+    length(Board, NumRows),
     % each row is of NumCols length
     % most examples use same_length/2 since the ordering of arguments in length/2 doesn't work well with maplist
     Board = [Row|Rows],
-    length(Row,NumCols),
-    maplist(same_length(Row),Rows).
+    length(Row, NumCols),
+    maplist(same_length(Row), Rows).
 
 
-fill(_,[]).
+fill(_, []).
 
-fill(Element,[Element|Rest]) :-
-    fill(Element,Rest).
+fill(Element, [Element|Rest]) :-
+    fill(Element, Rest).
 
 % count(Element, Count, List)
 % Count & fill List with the Element - unbound items will be filled with 's'
-count(_,0,[]).
+count(_, 0, []).
 
-count(Element,Count,[Element|Tail]) :-
+count(Element, Count, [Element|Tail]) :-
     % succ(essor?) is similar to TailCount is Count + 1
     % but with an extra check that TailCount >= 0
-    succ(TailCount,Count),
-    count(Element,TailCount,Tail).
+    succ(TailCount, Count),
+    count(Element, TailCount, Tail).
 
-count(Element,Count,[s|Tail]) :-
-    count(Element,Count,Tail).
+count(Element, Count, [s|Tail]) :-
+    count(Element, Count, Tail).
 
-
-build_mega_board(RowCounts,ColCounts,Board,MegaRowCounts,MegaColCounts,MegaBoard) :-
-    build_mega_row_counts(RowCounts,MegaRowCounts),
-    build_mega_row_counts(ColCounts,MegaColCounts),
-    length(MegaColCounts,NumMegaCols),
-    length(WallRow,NumMegaCols),
-    count(w,NumMegaCols,WallRow),
-    build_mega_rows(Board,MegaRows),
-    append([WallRow|MegaRows],[WallRow],MegaBoard).
-
-build_mega_row_counts(RowCounts,MegaRowCounts) :-
-    length(RowCounts,NumRows),
-    % Build with the original NumRows then add 2 to everything
-    append([NumRows|RowCounts],[NumRows],TempRowCounts),
-    maplist(plus(2),TempRowCounts,MegaRowCounts).
-
-build_mega_rows(Rows,MegaRows) :-
-    maplist(build_mega_row,Rows,MegaRows).
-
-build_mega_row(Row,MegaRow) :-
-    append([w|Row],[w],MegaRow).
+count(Element, Count, [Head|Tail]) :-
+    nonvar(Head),
+    Head \= Element,
+    count(Element, Count, Tail).
 
 
-rule_lines(RowCounts,ColCounts,Rows) :-
-    transpose(Rows,Cols),
-    append(RowCounts,ColCounts,LineCounts),
-    append(Rows,Cols,Lines),
+build_mega_board(Board, MegaBoard) :-
+    Board = [Row|_],
+    length(Row, NumCols),
+    NumMegaCols is NumCols + 2,
+    length(WallRow, NumMegaCols),
+    count(w, NumMegaCols, WallRow),
+    maplist(build_mega_row, Board, MegaRows),
+    append([WallRow|MegaRows], [WallRow], MegaBoard).
+
+build_mega_row(Row, MegaRow) :-
+    append([w|Row], [w], MegaRow).
+
+
+rule_lines(RowCounts, ColCounts, Rows) :-
+    transpose(Rows, Cols),
+    append(RowCounts, ColCounts, LineCounts),
+    append(Rows, Cols, Lines),
     % Sum the number of used cells (or potential wall combos) [chests use 3 cells]
     pack(LineCounts, Lines, Pack),
     % Sort by the least options to most options (combine rows & columns and their counts)
-    sort(Pack,SortedPack),
-    % Fill in rows (& columns) with w & s, decrementing count for each w [count(w,Count,List)]
+    sort(Pack, SortedPack),
+    % Fill in rows (& columns) with w & s, decrementing count for each w [count(w, Count, List)]
     rule_lines_(SortedPack).
 
 pack([], [], []).
@@ -92,11 +88,11 @@ rule_lines_([line(_, LineCount, Line)|Rest]) :-
     rule_lines_(Rest).
 
 
-print_board(Code,Name,RowCounts,ColCounts,Board) :-
-    write('   '),writeln(Code),
-    write('   '),writeln(Name),
-    write('   '),print_col_counts(ColCounts),
-    print_row_counts(RowCounts,Board).
+print_board(Code, Name, RowCounts, ColCounts, Board) :-
+    write('   '), writeln(Code),
+    write('   '), writeln(Name),
+    write('   '), print_col_counts(ColCounts),
+    print_row_counts(RowCounts, Board).
 
 print_col_counts([]) :-
     nl.
@@ -106,14 +102,14 @@ print_col_counts([ColCount|ColCounts]) :-
     write(' '),
     print_col_counts(ColCounts).
 
-print_row_counts([],[]) :-
+print_row_counts([], []) :-
     nl.
 
-print_row_counts([RowCount|RowCounts],[Row|Rows]) :-
+print_row_counts([RowCount|RowCounts], [Row|Rows]) :-
     write(RowCount),
     write(' |'),
     print_row(Row),
-    print_row_counts(RowCounts,Rows).
+    print_row_counts(RowCounts, Rows).
 
 print_row([]) :-
     nl.
@@ -133,9 +129,9 @@ print_element(Element) :-
 
 
 solve_puzzle(Code) :-
-    puzzle(Code,Name,RowCounts,ColCounts,Board),
-    dnd(RowCounts,ColCounts,Board),
-    print_board(Code,Name,RowCounts,ColCounts,Board).
+    puzzle(Code, Name, RowCounts, ColCounts, Board),
+    dnd(RowCounts, ColCounts, Board),
+    print_board(Code, Name, RowCounts, ColCounts, Board).
 
 
 puzzle('f.1', "adventurer's guide",
--- dnd.plt
+++ dnd.plt
@@ -18,8 +18,8 @@ test(member, [nondet]) :-
 test(count_8_w_8, all(Row == [
             [w,w,w,w,w,w,w,w]
         ])) :-
-    length(Row,8),
-    count(w,8,Row).
+    Row = [_,_,_,_,_,_,_,_],
+    count(w, 8, Row).
 
 test(count_8_w_7, all(Row == [
             [w,w,w,w,w,w,w,s],
@@ -31,8 +31,8 @@ test(count_8_w_7, all(Row == [
             [w,s,w,w,w,w,w,w],
             [s,w,w,w,w,w,w,w]
         ])) :-
-    length(Row,8),
-    count(w,7,Row).
+    Row = [_,_,_,_,_,_,_,_],
+    count(w, 7, Row).
 
 test(count_8_w_1, all(Row == [
             [w,s,s,s,s,s,s,s],
@@ -44,14 +44,32 @@ test(count_8_w_1, all(Row == [
             [s,s,s,s,s,s,w,s],
             [s,s,s,s,s,s,s,w]
         ])) :-
-    length(Row,8),
-    count(w,1,Row).
+    Row = [_,_,_,_,_,_,_,_],
+    count(w, 1, Row).
 
 test(count_8_w_0, all(Row == [
             [s,s,s,s,s,s,s,s]
         ])) :-
-    length(Row,8),
-    count(w,0,Row).
+    Row = [_,_,_,_,_,_,_,_],
+    count(w, 0, Row).
+
+test(count_8_w_7_m, all(Row == [
+            [w,w,w,w,w,m,w,w]
+        ])) :-
+    Row = [_,_,_,_,_,m,_,_],
+    count(w, 7, Row).
+
+test(count_8_w_1_m, all(Row == [
+            [w,s,m,s,s,s,s,s],
+            [s,w,m,s,s,s,s,s],
+            [s,s,m,w,s,s,s,s],
+            [s,s,m,s,w,s,s,s],
+            [s,s,m,s,s,w,s,s],
+            [s,s,m,s,s,s,w,s],
+            [s,s,m,s,s,s,s,w]
+        ])) :-
+    Row = [_,_,m,_,_,_,_,_],
+    count(w, 1, Row).
 
 :- end_tests(count).
 
@@ -61,7 +79,7 @@ test(count_8_w_0, all(Row == [
 % We can't use Board == [...] because of the free variables.
 % Use Board = [...] to check it unifies.
 test(build_board_6x6_empty) :-
-    build_board(6,6,Board),
+    build_board(6, 6, Board),
     Board = [
         [_,_,_,_,_,_],
         [_,_,_,_,_,_],
@@ -80,7 +98,7 @@ test(build_board_6x6_partial) :-
         [_,_,_,_,_,m],
         [m|_]
     ],
-    build_board(6,6,Board),
+    build_board(6, 6, Board),
     Board = [
         [_,_,_,_,_,c],
         [_,_,_,_,_,_],
@@ -91,7 +109,7 @@ test(build_board_6x6_partial) :-
     ].
 
 test(build_board_8x8_empty) :-
-    build_board(8,8,Board),
+    build_board(8, 8, Board),
     Board = [
         [_,_,_,_,_,_,_,_],
         [_,_,_,_,_,_,_,_],
@@ -114,7 +132,7 @@ test(build_board_8x8_partial) :-
         _,
         [_,_,_,_,_,_,_,m]
     ],
-    build_board(8,8,Board),
+    build_board(8, 8, Board),
     Board = [
         [_,_,_,_,_,_,_,_],
         [_,_,_,_,_,_,_,m],
@@ -132,8 +150,6 @@ test(build_board_8x8_partial) :-
 :- begin_tests(mega_board).
 
 test(build_mega_board_8x8_empty, nondet) :-
-    RowCounts = [1,2,3,4,5,6,7,8],
-    ColCounts = [7,6,5,4,3,2,1,0],
     Board = [
         [A1,A2,A3,A4,A5,A6,A7,A8],
         [B1,B2,B3,B4,B5,B6,B7,B8],
@@ -144,9 +160,7 @@ test(build_mega_board_8x8_empty, nondet) :-
         [G1,G2,G3,G4,G5,G6,G7,G8],
         [H1,H2,H3,H4,H5,H6,H7,H8]
     ],
-    build_mega_board(RowCounts,ColCounts,Board,MegaRowCounts,MegaColCounts,MegaBoard),
-    MegaRowCounts == [10, 3, 4, 5, 6, 7, 8, 9,10,10],
-    MegaColCounts == [10, 9, 8, 7, 6, 5, 4, 3, 2,10],
+    build_mega_board(Board, MegaBoard),
     MegaBoard == [
         [ w, w, w, w, w, w, w, w, w, w],
         [ w,A1,A2,A3,A4,A5,A6,A7,A8, w],
@@ -160,11 +174,6 @@ test(build_mega_board_8x8_empty, nondet) :-
         [ w, w, w, w, w, w, w, w, w, w]
     ].
 
-test(build_mega_row_counts) :-
-    RowCounts = [0,1,2,3,4,5,6,7,8,9],
-    build_mega_row_counts(RowCounts,MegaRowCounts),
-    MegaRowCounts = [12, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12].
-
 :- end_tests(mega_board).
 
 
@@ -173,8 +182,8 @@ test(build_mega_row_counts) :-
 test(rule_lines_simple_case, nondet) :-
     RowCounts = [8,7,6,5,4,3,2,1],
     ColCounts = [8,7,6,5,4,3,2,1],
-    build_board(8,8,Board),
-    rule_lines(RowCounts,ColCounts,Board),
+    build_board(8, 8, Board),
+    rule_lines(RowCounts, ColCounts, Board),
     Board == [
         [w,w,w,w,w,w,w,w],
         [w,w,w,w,w,w,w,s],
@@ -189,8 +198,8 @@ test(rule_lines_simple_case, nondet) :-
 test(rule_lines_worst_case, nondet) :-
     RowCounts = [1,2,3,4,5,6,7,8],
     ColCounts = [1,2,3,4,5,6,7,8],
-    build_board(8,8,Board),
-    rule_lines(RowCounts,ColCounts,Board),
+    build_board(8, 8, Board),
+    rule_lines(RowCounts, ColCounts, Board),
     Board == [
         [s,s,s,s,s,s,s,w],
         [s,s,s,s,s,s,w,w],
